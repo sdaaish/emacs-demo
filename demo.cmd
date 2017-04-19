1dec5ae58d39db51870175378eb2b39b569aad15
Stig Dahl <Stig@charlottendal.net>
2017-04-19 21:30:58 +0200

    Initial commit, created structure.

---
+++ demo.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+:: Starts emacs with different config than normal
+:: 2017-04-19/SDAA
+
+if [%1]==[] goto usage
+
+if not exist %1 (
+   echo File %1 does not exist!
+   exit /b 1
+)
+
+setlocal
+set $file=%~f1
+set $dir=%~dp1
+echo %$file% %$dir%
+emacs --no-init-file --chdir %$dir% --load %$file% %$file%
+
+goto end
+:usage
+echo "Usage: transfer-vt <filename>"
+exit /b 2
+
+:end
+endlocal
