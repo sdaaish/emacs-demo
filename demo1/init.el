4918969311ebd4962d092c25b18750eecc814964
Stig Dahl <Stig@charlottendal.net>
2017-06-29 21:53:47 +0200

    Fixed formatting

--- demo1/init.el
+++ demo1/init.el
@@ -6,6 +6,7 @@
 (setq user-emacs-directory (file-name-directory user-init-file))
 (setq default-directory (file-name-directory user-init-file))
 (setq package-user-dir (concat (file-name-directory user-init-file) "elpa/"))
+
 ;; use a separate file to store changes made by customize.
 (setq custom-file (expand-file-name "customize.el" user-emacs-directory))
 (load custom-file 'noerror)
