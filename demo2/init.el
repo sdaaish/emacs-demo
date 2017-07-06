7ab26f4343850127d45a56d997b8beb37cf723e9
Stig Dahl <stig@charlottendal.net>
2017-07-06 13:58:08 +0200

    Added helm-stuff for this config.

---
+++ demo2/init.el
@@ -0,0 +1,93 @@
+;; This is the init-file that are loaded by emacs when the script demo.cmd are run.
+;; All files used by this emacs-session (packages, themes, faces etc) should be stored in this directory.
+
+;; This parts sets variables so files are stored in the init-file directory
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq user-emacs-directory (file-name-directory user-init-file))
+(setq default-directory (file-name-directory user-init-file))
+(setq package-user-dir (concat (file-name-directory user-init-file) "elpa/"))
+
+;; use a separate file to store changes made by customize.
+(setq custom-file (expand-file-name "customize.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+(require 'package)
+(package-initialize)
+(package-refresh-contents)
+
+;; Use UTF-8 as preferred coding-system
+;; Useful for package-archives.
+(prefer-coding-system       'utf-8)
+
+;; Set what kind av archives shpuld be used
+(setq package-enable-at-startup nil)
+(setq package-archives nil)
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Bytecompile packages
+(eval-when-compile
+  (require 'use-package))
+
+;; Make sure packages are installed
+(setq use-package-always-ensure t)
+
+;; Load these packages
+(use-package helm)
+(use-package google)
+(use-package helm-google
+  :bind ("C-c C--" . helm-google-suggest))
+
+;; Config for Helm.
+;; Example from https://tuhdo.github.io/helm-intro.html
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
