af4ec35620922dc9bce2484054fef890faacc7b3
Stig Dahl <stig@charlottendal.net>
2026-03-01 20:36:31 +0100

    Initial settings

--- lsp/init.el
+++ lsp/init.el
@@ -1,4 +1,4 @@
-
+;; Early init
 ;; This parts sets variables so files are stored in the init-file directory
 
 ;; [[file:init.org::*Early init][Early init:1]]
@@ -15,7 +15,7 @@
 (set-language-environment "utf-8")
 ;; Early init:1 ends here
 
-
+;; Install straight package manager
 ;; Use =straight= as package-manager instead for the built in.
 
 ;; [[file:init.org::*Install straight package manager][Install straight package manager:1]]
@@ -33,7 +33,7 @@
   (load bootstrap-file nil 'nomessage))
 ;; Install straight package manager:1 ends here
 
-
+;; Also install use-package
 ;; =Use-package= still works with =straight=, lets use that for simplified configuration.
 
 ;; [[file:init.org::*Also install use-package][Also install use-package:1]]
@@ -41,14 +41,16 @@
 (setq straight-use-package-by-default t)
 ;; Also install use-package:1 ends here
 
-
+;; GCMH
 ;; Manage garbage collection (=gh=) automatically.
 
 ;; [[file:init.org::*GCMH][GCMH:1]]
-  (use-package gcmh
-    :diminish gcmh-mode
-    :config (gcmh-mode 1))
+(use-package gcmh
+  :diminish gcmh-mode
+  :config (gcmh-mode 1))
 ;; GCMH:1 ends here
+
+;; Syntax checking with the modern version of flymake
 
 ;; [[file:init.org::*Syntax checking with the modern version of flymake][Syntax checking with the modern version of flymake:1]]
 (use-package flycheck
@@ -56,18 +58,22 @@
   (global-flycheck-mode t))
 ;; Syntax checking with the modern version of flymake:1 ends here
 
+;; Navigation with avy
+
 ;; [[file:init.org::*Navigation with avy][Navigation with avy:1]]
 (use-package avy-flycheck
   :config
   (avy-flycheck-setup))
 ;; Navigation with avy:1 ends here
 
+;; Snippets
+
 ;; [[file:init.org::*Snippets][Snippets:1]]
 (use-package yasnippet)
 (use-package yasnippet-snippets)
 ;; Snippets:1 ends here
 
-
+;; Language Server Protocol
 ;; Has built-in support for bash. Needs bash-language-server, installed separately
 
 ;; [[file:init.org::*Language Server Protocol][Language Server Protocol:1]]
@@ -78,25 +84,35 @@
   (lsp-mode . lsp-enable-which-key-integration))
 ;; Language Server Protocol:1 ends here
 
+;; LSP for powershell
+
 ;; [[file:init.org::*LSP for powershell][LSP for powershell:1]]
 (use-package powershell)
 (use-package ob-powershell)
 ;; LSP for powershell:1 ends here
+
+;; UI for LSP
 
 ;; [[file:init.org::*UI for LSP][UI for LSP:1]]
 (use-package lsp-ui
   :commands lsp-ui-mode)
 ;; UI for LSP:1 ends here
 
+;; Ivy
+
 ;; [[file:init.org::*Ivy][Ivy:1]]
 (use-package ivy
   :config (ivy-mode 1))
 ;; Ivy:1 ends here
 
+;; Ivy for LSP
+
 ;; [[file:init.org::*Ivy for LSP][Ivy for LSP:1]]
 (use-package lsp-ivy
   :commands lsp-ivy-workspace-symbol)
 ;; Ivy for LSP:1 ends here
+
+;; Which key
 
 ;; [[file:init.org::*Which key][Which key:1]]
 (use-package which-key
@@ -104,25 +120,32 @@
   (which-key-mode))
 ;; Which key:1 ends here
 
+;; Autocompletion with Company
+
 ;; [[file:init.org::*Autocompletion with Company][Autocompletion with Company:1]]
-(use-package company)
+(use-package company
+  :custom (company-minimum-prefix-length 1)
+  (company-idle-time 0.2))
+
 (use-package company-lsp
   :config
   (push 'company-lsp company-backends))
 ;; Autocompletion with Company:1 ends here
 
-
+;; Performance
 ;; Increase buffer to read from LSP process.
 
 ;; [[file:init.org::*Performance][Performance:1]]
 (setq read-process-output-max (* 1024 1024))
 ;; Performance:1 ends here
 
+;; Org mode
+
 ;; [[file:init.org::*Org mode][Org mode:1]]
 (setq org-src-preserve-indentation t)
 ;; Org mode:1 ends here
 
-
+;; Load modus theme
 ;; =Modus themes= is a collection  of highly customizable themes.
 
 ;; [[file:init.org::*Load modus theme][Load modus theme:1]]
@@ -131,7 +154,7 @@
   :init (load-theme 'modus-vivendi-tinted t))
 ;; Load modus theme:1 ends here
 
-
+;; Set a default font.
 ;; Set a default font, and change also the current frame.
 
 ;; [[file:init.org::*Set a default font.][Set a default font.:1]]
@@ -139,7 +162,7 @@
 (set-frame-font "Lucida Console 12" nil t)
 ;; Set a default font.:1 ends here
 
-
+;; The end
 ;; Print out startup time.
 
 ;; [[file:init.org::*The end][The end:1]]
